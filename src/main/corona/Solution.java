e9c43a908204edf3b2287c5231abceb86597c871
ofir511996 <ofir511996@gmail.com>
2020-04-28 13:31:56 +0300

    finish Basic API

--- src/main/corona/Solution.java
+++ src/main/corona/Solution.java
@@ -6,132 +6,311 @@ import corona.business.Lab;
 import corona.business.ReturnValue;
 import corona.business.Vaccine;
 import corona.data.DBConnector;
+import corona.data.PostgreSQLErrorCodes;
 
 import java.sql.Connection;
+import java.sql.Statement;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-import static corona.business.ReturnValue.OK;
+import static corona.business.ReturnValue.*;
+
+
 
 
 public class Solution {
-    public static PreparedStatement prepare(String quary) throws SQLException {
-        Connection connection = DBConnector.getConnection();
-        PreparedStatement pstmt = null;
-        return connection.prepareStatement(quary);
+    static class Utils {
+        public static void closeQuietly(Connection connection) {
+            try {
+                connection.close();
+            } catch (SQLException e) { // NOPMD
+                // quiet
+            }
+        }
+
+        public static void closeQuietly(Statement stmt) {
+            try {
+                stmt.close();
+            } catch (SQLException e) { // NOPMD
+                // quiet
+            }
+        }
+
+        public static void closeQuietly(ResultSet rs) {
+            try {
+                if(rs!=null)
+                    rs.close();
+            } catch (SQLException e) { // NOPMD
+                // quiet
+            }
+        }
+        public static ReturnValue get_retval(SQLException e) {
+            if (Integer.parseInt(e.getSQLState()) == PostgreSQLErrorCodes.CHECK_VIOLATION.getValue())
+                return BAD_PARAMS;
+            else if (Integer.parseInt(e.getSQLState()) == PostgreSQLErrorCodes.NOT_NULL_VIOLATION.getValue())
+                return BAD_PARAMS;
+            else if (Integer.parseInt(e.getSQLState()) == PostgreSQLErrorCodes.FOREIGN_KEY_VIOLATION.getValue())
+                return BAD_PARAMS;
+            else if (Integer.parseInt(e.getSQLState()) == PostgreSQLErrorCodes.UNIQUE_VIOLATION.getValue())
+                return ALREADY_EXISTS;
+            else
+                return ERROR;
+        }
     }
-    public static void execute(PreparedStatement pstmt) throws SQLException {
-        Connection connection = DBConnector.getConnection();
-        SQLException exep=null;
+
+    static class DB {
+        Connection connection=null;
+        PreparedStatement pstmt=null;
+        ResultSet results=null;
+
+        public void prepare(String sql) throws SQLException {
+            connection = DBConnector.getConnection();
+            pstmt = connection.prepareStatement(sql);
+        }
+
+        public void close(){
+            Utils.closeQuietly(results);
+            Utils.closeQuietly(pstmt);
+            Utils.closeQuietly(connection);
+
+
+        }
+
+        public void get_record(String sql, int id) throws SQLException {
+            prepare(sql);
+            pstmt.setInt(1, id);
+            results = pstmt.executeQuery();
+        }
+
+        public ReturnValue exists(String sql, int id) {
+            ReturnValue r = OK;
+            try {
+                get_record(sql, id);
+                if (results.getFetchSize() == 0)
+                    r = NOT_EXISTS;
+            } catch (SQLException e) {
+                e.printStackTrace();
+                r = ERROR;
+            }
+            return r;
+        }
+    }
+
+    public static void createTables() {
+        Connection connection = null;
+        PreparedStatement pstmt=null;
         try {
+            connection = DBConnector.getConnection();
+            pstmt=connection.prepareStatement("CALL public.CreateDB();");//TODO: copy
             pstmt.execute();
         } catch (SQLException e) {
-            exep=e;
+            e.printStackTrace();
         }
         finally {
-            try {
-                pstmt.close();
-            } catch (SQLException e) {
-                if(exep == null)
-                    exep=e;
-            }
-            try {
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (SQLException e) {
-                if(exep == null)
-                    exep=e;
-            }
-            if(exep!=null)
-                throw exep;
-        }
-    }
-    public static void createTables() {
-        try {
-            execute(prepare("CALL public.CreateDB();"));//TODO: copy
-        } catch (SQLException e) {
-            e.printStackTrace();
+            Utils.closeQuietly(pstmt);
+            Utils.closeQuietly(connection);
         }
     }
 
     public static void clearTables() {
+        Connection connection = null;
+        PreparedStatement pstmt=null;
         try {
-            execute(prepare("CALL public.TruncateDB();"));//TODO: copy
+            connection = DBConnector.getConnection();
+            pstmt=connection.prepareStatement("CALL public.TruncateDB();");//TODO: copy
+            pstmt.execute();
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        finally {
+            Utils.closeQuietly(pstmt);
+            Utils.closeQuietly(connection);
+        }
     }
     public static void dropTables() {
+        Connection connection = null;
+        PreparedStatement pstmt=null;
         try {
-            execute(prepare("CALL public.DropDB();"));//TODO: copy
+            connection = DBConnector.getConnection();
+            pstmt=connection.prepareStatement("CALL public.DropDB();");//TODO: copy
+            pstmt.execute();
         } catch (SQLException e) {
             e.printStackTrace();
+        }
+        finally {
+            Utils.closeQuietly(pstmt);
+            Utils.closeQuietly(connection);
         }
     }
 
     public static ReturnValue addLab(Lab lab) {
+        ReturnValue ret=OK;
+        DB db= new DB();
         try {
-            PreparedStatement pstmt = prepare("INSERT INTO public.labs(\n" +
+            db.prepare("INSERT INTO public.labs(\n" +
                     "\tlabid, name, city, active)\n" +
                     "\tVALUES (?, ?, ?, ?);");
-            pstmt.setInt(1, lab.getId());
-            pstmt.setString(2, lab.getName());
-            pstmt.setString(3, lab.getCity());
-            pstmt.setBoolean(4, lab.getIsActive());
-            execute(pstmt);
+            db.pstmt.setInt(1, lab.getId());
+            db.pstmt.setString(2, lab.getName());
+            db.pstmt.setString(3, lab.getCity());
+            db.pstmt.setBoolean(4, lab.getIsActive());
+            db.pstmt.execute();
         } catch (SQLException e) {
-            e.printStackTrace();
+            ret= Utils.get_retval(e);
         }
-        return OK;
+        finally {
+            db.close();
+        }
+        return ret;
     }
 
     public static Lab getLabProfile(Integer labID) {
-        return new Lab();
+        DB db= new DB();
+        Lab lab=new Lab();
+        try {
+            db.get_record("SELECT * FROM public.labs WHERE labID=?", labID);
+            lab.setId(db.results.getInt(1));
+            lab.setName(db.results.getString(2));
+            lab.setCity(db.results.getString(3));
+            lab.setIsActive(db.results.getBoolean(4));
+        } catch (SQLException e) {
+            lab= Lab.badLab();
+        } finally {
+            db.close();
+        }
+        return lab;
     }
 
     public static ReturnValue deleteLab(Lab lab) {
+        DB db= new DB();
+        ReturnValue ret = db.exists("SELECT labID FROM public.labs WHERE labID=?", lab.getId());
+        if(ret!=OK)
+            return ret;
         try {
-            PreparedStatement pstmt1 = prepare("SELECT labID FROM public.labs WHERE labID=?");
-            pstmt1.setInt(1, lab.getId());
-            ResultSet results = pstmt1.executeQuery();
-            if(results.getFetchSize()==0)
-                return ReturnValue.NOT_EXISTS;
-            PreparedStatement pstmt2 = prepare("DELETE FROM public.labs WHERE labID=?");
-            pstmt2.setInt(1, lab.getId());
-            execute(pstmt2);
+            db.prepare("DELETE FROM public.labs WHERE labID=?");
+            db.pstmt.setInt(1, lab.getId());
+            db.pstmt.execute();
         } catch (SQLException e) {
-            e.printStackTrace();
-            return  ReturnValue.ERROR;
-
+            ret= Utils.get_retval(e);
         }
-        return OK;
+        finally {
+            db.close();
+        }
+        return ret;
     }
 
     public static ReturnValue addEmployee(Employee employee) {
-        return OK;
+        ReturnValue ret=OK;
+        DB db= new DB();
+        try {
+            db.prepare("INSERT INTO public.employees(\n" +
+                    "\temployeeid, name, birth_city)\n" +
+                    "\tVALUES (?, ?, ?);");
+            db.pstmt.setInt(1, employee.getId());
+            db.pstmt.setString(2, employee.getName());
+            db.pstmt.setString(3, employee.getCity());
+            db.pstmt.execute();
+        } catch (SQLException e) {
+            ret= Utils.get_retval(e);
+        }
+        finally {
+            db.close();
+        }
+        return ret;
     }
 
     public static Employee getEmployeeProfile(Integer employeeID) {
-        return new Employee();
+        DB db= new DB();
+        Employee employee=new Employee();
+        try {
+            db.get_record("SELECT * FROM public.employees WHERE employeeID=?", employeeID);
+            employee.setId(db.results.getInt(1));
+            employee.setName(db.results.getString(2));
+            employee.setCity(db.results.getString(3));
+        } catch (SQLException e) {
+            employee= Employee.badEmployee();
+        } finally {
+           db.close();
+        }
+        return employee;
     }
 
     public static ReturnValue deleteEmployee(Employee employee) {
-        return OK;
+        DB db= new DB();
+        ReturnValue ret = db.exists("SELECT employeeID FROM public.employees WHERE employeeID=?", employee.getId());
+        if(ret!=OK)
+            return ret;
+        try {
+            db.prepare("DELETE FROM public.employees WHERE employeeID=?");
+            db.pstmt.setInt(1, employee.getId());
+            db.pstmt.execute();
+        } catch (SQLException e) {
+            ret= Utils.get_retval(e);
+        }
+        finally {
+            db.close();
+        }
+        return ret;
     }
 
     public static ReturnValue addVaccine(Vaccine vaccine) {
-        return OK;
+        ReturnValue ret=OK;
+        DB db= new DB();
+        try {
+            db.prepare("INSERT INTO public.vaccines(\n" +
+                    "\tvaccineid, name, cost, units_in_stock, productivity)\n" +
+                    "\tVALUES (?, ?, ?, ?, ?);");
+            db.pstmt.setInt(1, vaccine.getId());
+            db.pstmt.setString(2, vaccine.getName());
+            db.pstmt.setInt(3, vaccine.getCost());
+            db.pstmt.setInt(4, vaccine.getUnits());
+            db.pstmt.setInt(5, vaccine.getProductivity());
+            db.pstmt.execute();
+        } catch (SQLException e) {
+            ret= Utils.get_retval(e);
+        }
+        finally {
+            db.close();
+        }
+        return ret;
     }
 
     public static Vaccine getVaccineProfile(Integer vaccineID) {
-        return new Vaccine();
+        DB db= new DB();
+        Vaccine vaccine=new Vaccine();
+        try {
+            db.get_record("SELECT * FROM public.vaccines WHERE vaccineID=?", vaccineID);
+            vaccine.setId(db.results.getInt(1));
+            vaccine.setName(db.results.getString(2));
+            vaccine.setCost(db.results.getInt(3));
+            vaccine.setUnits(db.results.getInt(41));
+            vaccine.setProductivity(db.results.getInt(5));
+        } catch (SQLException e) {
+            vaccine= Vaccine.badVaccine();
+        } finally {
+            db.close();
+        }
+        return vaccine;
     }
 
     public static ReturnValue deleteVaccine(Vaccine vaccine) {
-        return OK;
+        DB db= new DB();
+        ReturnValue ret = db.exists("SELECT vaccineID FROM public.vaccines WHERE vaccineID=?", vaccine.getId());
+        if(ret!=OK)
+            return ret;
+        try {
+            db.prepare("DELETE FROM public.vaccines WHERE vaccineID=?");
+            db.pstmt.setInt(1, vaccine.getId());
+            db.pstmt.execute();
+        } catch (SQLException e) {
+            ret= Utils.get_retval(e);
+        }
+        finally {
+            db.close();
+        }
+        return ret;
     }
 
     public static ReturnValue employeeJoinLab(Integer employeeID, Integer labID, Integer salary) {
